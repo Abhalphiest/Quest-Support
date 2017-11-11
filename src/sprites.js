4971f316984e4c8569e6d06399087fcc90e99714
Margaret Dorsey <margaretdorsey94@gmail.com>
2017-11-11 02:38:22 -0500

    added starter files

---
+++ src/sprites.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// -------------------------------------------
+//
+// sprites.js composes sprites out of various parts
+//
+// -------------------------------------------
+
+var app = app || {};
